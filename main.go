f2365e2514a9f7ca96f1323d03012fdf2dca92f8
gongkaixuan <gkx826@163.com>
2022-04-29 11:13:45 +0800

    新增关于页面

--- main.go
+++ main.go
@@ -5,8 +5,20 @@ import (
 	"net/http"
 )
 
+/*
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>hello 这里是 goblog</h1>")
+	fmt.Fprint(w,"请求路径为："+r.URL.Path)
+}
+*/
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>hello 这里是 goblog</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记！")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到！</h1>")
+	}
 }
 func main() {
 	http.HandleFunc("/", handlerFunc)
