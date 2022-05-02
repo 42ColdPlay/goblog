7d7e91f40f3c05aaf376f103bf32ceb716fbaa7f
gongkaixuan <gkx826@163.com>
2022-05-02 22:03:38 +0800

    连接数据库

--- main.go
+++ main.go
@@ -1,17 +1,54 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 	"unicode/utf8"
 
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 var router = mux.NewRouter()
+var db *sql.DB
+
+func initDB() {
+	var err error
+	config := mysql.Config{
+		User:                 "root",
+		Passwd:               "gong97826",
+		Addr:                 "127.0.0.1:3306",
+		Net:                  "tcp",
+		DBName:               "goblog",
+		AllowNativePasswords: true,
+	}
+	//准备数据库连接池
+	db, err = sql.Open("mysql", config.FormatDSN())
+	checkError(err)
+	//设置最大连接数
+	db.SetMaxOpenConns(25)
+	//设置最大空闲连接数
+	db.SetMaxIdleConns(25)
+	//设置每个连接的过期时间
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	//尝试连接，失败报错
+	err = db.Ping()
+	checkError(err)
+
+}
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 /*
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -173,6 +210,7 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 func main() {
+	initDB()
 
 	//Name() 方法用来给路由命名
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
